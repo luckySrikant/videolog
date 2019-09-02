66895832ae2160a3309698e8779ee1413b5c7cac
luckySrikant <51993959+luckySrikant@users.noreply.github.com>
2019-09-02 20:30:18 +0530

    Update index.js

--- video/index.js
+++ video/index.js
@@ -1,20 +1,19 @@
 
 function playPause(btn,vid){
 	var vid = document.getElementById(vid);
-	
+	let data=`video is ${(vid.paused)?`paused`:`play`} at ${vid.currentTime} `;
 	if(vid.paused){
 		vid.play();
 		btn.innerHTML = "Pause";
-		// fs.appendFile('message.txt', vid.currentTime, (err) => {
-        //     if (err) throw err;
-        //     console.log('The "data to append" was appended to file!');
-        //   });
-        file=fopen("message.txt",0);
 		alert("the video is play at"+vid.currentTime);
 	} else {
 		vid.pause();
 		btn.innerHTML = "Play";
 		alert("the video is paused at"+vid.currentTime);
-	}
+    }
+  let a=  document.createElement("p");
+    a.innerHTML=data;
+    document.getElementById("div1").append(a);
+    
 	
 }
